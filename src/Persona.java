6d7d2afd2bd6f5baa94d95b6a3f6102685f96429
Adrian <williampeleas@gmail.com>
2019-05-27 08:38:45 +0200

    JavaDoc

--- src/Persona.java
+++ src/Persona.java
@@ -1,10 +1,23 @@
-
+/**
+ * 
+ * @author Nacho, Adrián, Miguelangel, Gerardo
+ *
+ */
 public class Persona {
 
+	/**
+	 * Atributos de la clase persona
+	 */
 	private String nombre;
 	private String apellidos;
 	private String dni;
 	
+	/**
+	 * Constructor de la clase
+	 * @param nombre Nombre de la persona
+	 * @param apellidos Apellidos de la perona
+	 * @param dni DNI de la persona
+	 */
 	public Persona(String nombre, String apellidos, String dni) {
 		super();
 		this.nombre = nombre;
@@ -12,26 +25,50 @@ public class Persona {
 		this.dni = dni;
 	}
 
+	/**
+	 * Devuelve el nombre de la persona
+	 * @return El nombre de la persona
+	 */
 	public String getNombre() {
 		return nombre;
 	}
 
+	/**
+	 * Establece el nombre de la persona
+	 * @param nombre El nombre de la persona
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
 
+	/**
+	 * Devuelve los apellidos de la persona
+	 * @return Los apellidos de la persona
+	 */
 	public String getApellidos() {
 		return apellidos;
 	}
 
+	/**
+	 * Establece los apellidos de la persona
+	 * @param apellidos Los apellidos de la persona
+	 */
 	public void setApellidos(String apellidos) {
 		this.apellidos = apellidos;
 	}
 
+	/**
+	 * Devuelve el DNI de la persona
+	 * @return El DNI de la persona
+	 */
 	public String getDni() {
 		return dni;
 	}
 
+	/**
+	 * Establece el DNI de la persona
+	 * @param dni El DNI de la persona
+	 */
 	public void setDni(String dni) {
 		this.dni = dni;
 	}
